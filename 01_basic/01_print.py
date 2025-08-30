667ef21648e19c4c4192e545ce6f21973362ea80
Adrian Garcia <adrian.garcia.gomez96@gmail.com>
2025-08-30 20:02:05 +0200

    update curso

---
+++ 01_basic/01_print.py
@@ -0,0 +1,16 @@
+###
+# 01 - print()
+# El módulo print() es el módulo que nos permite imprimir en consola.
+# Sirve para mostrar información en consola y te va a acompañar
+# TODA TU VIDA. Desde hoy hasta el fin de los tiempos
+###
+
+print("¡Hola, Twitch!")
+print('Esto tambien funciona con una comilla simple')
+
+print("Python", "es", "genial")
+
+print("Python", "es", "brutal", sep = "-")
+
+print("Esto se imprime", end = " ")
+print("en una linea")
